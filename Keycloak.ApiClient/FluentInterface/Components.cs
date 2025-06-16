554eb493802d0431bf1fe6ef051d3a427d10f85a
Alexander Braum <alexander.braum@gmail.com>
2025-06-16 11:54:33 +0200

    Add Component FI.

---
+++ Keycloak.ApiClient/FluentInterface/Components.cs
@@ -0,0 +1,70 @@
+﻿using keycloak;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keycloak.ApiClient.FluentInterface
+{
+    public class Component
+    {
+        public string Name => Representation?.Name ?? string.Empty;
+        public string Id => Representation?.Id ?? string.Empty;
+        public ComponentRepresentation Representation { get; set; }
+        public Realm Realm { get; set; }
+
+        public Component(
+            Realm realm
+            )
+        {
+            Realm = realm;
+        }
+    }
+
+    public static partial class RealmExtensions
+    {
+        public async static Task<ICollection<Component>> GetAllComponentsAsync(this Realm realm, string name = null, string parent = null, string type = null)
+        {
+            var data = await realm.Client.AdminRealmsComponentsGetAsync(realm.Name, name, parent, type);
+            var result = data.Result.Select(x => realm.GetComponentObject(x)).ToList();
+            return result;
+        }
+
+        public async static Task<Component> GetComponentAsync(this Realm realm, string id)
+        {
+            var data = await realm.Client.AdminRealmsComponentsGetAsync(id: id, realm: realm.Name);
+            var result = realm.GetComponentObject(data.Result);
+            return result;
+        }
+
+        public async static Task<Component> CreateComponentAsync(this Realm realm, ComponentRepresentation representation)
+        {
+            var data = await realm.Client.AdminRealmsComponentsPostAsync(realm.Name, representation);
+            var result = realm.GetComponentObject(representation);
+            return result;
+        }
+
+        private static Component GetComponentObject(this Realm realm, ComponentRepresentation representation)
+        {
+            var result = new Component(realm)
+            {
+                Representation = representation
+            };
+            return result;
+        }
+    }
+
+    public static partial class ComponentExtensions
+    {
+        public async static Task<Component> UpdateAsync(this Component obj)
+        {
+            await obj.Realm.Client.AdminRealmsComponentsPutAsync(id: obj.Id, realm: obj.Realm.Name, obj.Representation);
+            return obj;
+        }
+
+        public async static Task<Component> DeleteAsync(this Component obj)
+        {
+            await obj.Realm.Client.AdminRealmsComponentsDeleteAsync(id: obj.Id, realm: obj.Realm.Name);
+            return obj;
+        }
+    }
+}
