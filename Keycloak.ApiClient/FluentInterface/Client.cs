08dff61388f542801b82b7a4cb95fb277ccd54db
Alexander Braum <alexander.braum@gmail.com>
2025-09-29 11:26:44 +0200

    Update Client, Realm with GetDefaultRepresentation()

--- Keycloak.ApiClient/FluentInterface/Client.cs
+++ Keycloak.ApiClient/FluentInterface/Client.cs
@@ -44,6 +44,12 @@ namespace Keycloak.ApiClient.FluentInterface
             return result;
         }
 
+        public async static Task<Client> CreateClientAsync(this Realm realm, string clientName)
+        {
+            var result = await realm.CreateClientAsync(GetDefaultClientRepresentation(clientName));
+            return result;
+        }
+
         private static Client GetClientObject(this Realm realm, ClientRepresentation representation)
         {
             var result = new Client(realm)
@@ -51,6 +57,67 @@ namespace Keycloak.ApiClient.FluentInterface
                 Representation = representation
             };
             return result;
+        }
+
+        public static ClientRepresentation GetDefaultClientRepresentation(string clientName)
+        {
+            return new ClientRepresentation
+            {
+                Id = clientName,
+                ClientId = clientName,
+                Name = clientName,
+                Description = "",
+                RootUrl = "",
+                AdminUrl = "",
+                BaseUrl = "",
+                Enabled = true,
+                ClientAuthenticatorType = "client-secret",
+                RedirectUris = new List<string> { "/*" },
+                WebOrigins = new List<string> { "/*" },
+                NotBefore = 0,
+                StandardFlowEnabled = true,
+                ImplicitFlowEnabled = true,
+                DirectAccessGrantsEnabled = true,
+                PublicClient = true,
+                FrontchannelLogout = true,
+                Protocol = "openid-connect",
+                Attributes = new Dictionary<string, string>
+                {
+                    { "realm_client", "false" },
+                    { "oidc.ciba.grant.enabled", "false" },
+                    { "backchannel.logout.session.required", "true" },
+                    { "standard.token.exchange.enabled", "false" },
+                    { "oauth2.device.authorization.grant.enabled", "false" },
+                    { "backchannel.logout.revoke.offline.tokens", "false" }
+                },
+                AuthenticationFlowBindingOverrides = new Dictionary<string, string>(),
+                FullScopeAllowed = true,
+                NodeReRegistrationTimeout = -1,
+                DefaultClientScopes = new List<string>
+                {
+                    "web-origins",
+                    "acr",
+                    "profile",
+                    "roles",
+                    "basic",
+                    "email"
+                },
+                OptionalClientScopes = new List<string>
+                {
+                    "address",
+                    "phone",
+                    "offline_access",
+                    "organization",
+                    "microprofile-jwt"
+                },
+                Access = new Dictionary<string, bool>
+                {
+                    { "view", true },
+                    { "configure", true },
+                    { "manage", true }
+                },
+                AdditionalProperties = new Dictionary<string, object>()
+            };
         }
     }
 
--- Keycloak.ApiClient/FluentInterface/Realm.cs
+++ Keycloak.ApiClient/FluentInterface/Realm.cs
@@ -21,7 +21,7 @@ namespace Keycloak.ApiClient.FluentInterface
     }
 
     public static partial class FluentKeycloakApiClientExtensions
-        {
+    {
         public async static Task<ICollection<Realm>> GetAllRealmsAsync(this FluentKeycloakApiClient client, bool? briefRepresentation = null)
         {
             var data = await client.GeneratedClient.AdminRealmsGetAsync(briefRepresentation);
@@ -45,6 +45,15 @@ namespace Keycloak.ApiClient.FluentInterface
             return realm;
         }
 
+        public async static Task<Realm> CreateRealmAsync(
+            this FluentKeycloakApiClient client,
+            string realmName)
+        {
+
+            var realm = await client.CreateRealmAsync(GetDefaultRealmRepresentation(realmName));
+            return realm;
+        }
+
         private static Realm GetRealmObject(this FluentKeycloakApiClient client, RealmRepresentation realmRepresentation)
         {
             var result = new Realm(client)
@@ -52,6 +61,147 @@ namespace Keycloak.ApiClient.FluentInterface
                 Representation = realmRepresentation
             };
             return result;
+        }
+
+        public static RealmRepresentation GetDefaultRealmRepresentation(string realmName)
+        {
+            return new RealmRepresentation
+            {
+                Id = realmName,
+                Realm = realmName,
+                DisplayName = realmName,
+                DisplayNameHtml = null,
+                DefaultSignatureAlgorithm = "RS256",
+                Enabled = true,
+                SslRequired = "external",
+                AccessTokenLifespan = 300,
+                AccessTokenLifespanForImplicitFlow = 900,
+                SsoSessionIdleTimeout = 1800,
+                SsoSessionMaxLifespan = 36000,
+                SsoSessionIdleTimeoutRememberMe = 0,
+                SsoSessionMaxLifespanRememberMe = 0,
+                OfflineSessionIdleTimeout = 2592000,
+                OfflineSessionMaxLifespanEnabled = false,
+                OfflineSessionMaxLifespan = 5184000,
+                ClientSessionIdleTimeout = 0,
+                ClientSessionMaxLifespan = 0,
+                ClientOfflineSessionIdleTimeout = 0,
+                ClientOfflineSessionMaxLifespan = 0,
+                AccessCodeLifespan = 60,
+                AccessCodeLifespanUserAction = 300,
+                AccessCodeLifespanLogin = 1800,
+                ActionTokenGeneratedByAdminLifespan = 43200,
+                ActionTokenGeneratedByUserLifespan = 300,
+                Oauth2DevicePollingInterval = 5,
+                RegistrationAllowed = false,
+                RegistrationEmailAsUsername = false,
+                RememberMe = false,
+                VerifyEmail = false,
+                LoginWithEmailAllowed = true,
+                DuplicateEmailsAllowed = false,
+                ResetPasswordAllowed = false,
+                EditUsernameAllowed = false,
+                BruteForceProtected = false,
+                PermanentLockout = false,
+                MaxTemporaryLockouts = 0,
+                BruteForceStrategy = BruteForceStrategy.MULTIPLE,
+                MaxFailureWaitSeconds = 900,
+                MinimumQuickLoginWaitSeconds = 60,
+                WaitIncrementSeconds = 60,
+                QuickLoginCheckMilliSeconds = 1000,
+                MaxDeltaTimeSeconds = 43200,
+                FailureFactor = 30,
+                DefaultRole = new RoleRepresentation
+                {
+                    Name = $"default-roles-{realmName}",
+                    Description = $"default-roles-{realmName}",
+                    Composite = true,
+                    ClientRole = false,
+                },
+                OtpPolicyType = "totp",
+                OtpPolicyAlgorithm = "HmacSHA1",
+                OtpPolicyInitialCounter = 0,
+                OtpPolicyDigits = 6,
+                OtpPolicyLookAheadWindow = 1,
+                OtpPolicyPeriod = 30,
+                OtpPolicyCodeReusable = false,
+                OtpSupportedApplications = new List<string>
+                {
+                    "totpAppFreeOTPName",
+                    "totpAppGoogleName",
+                    "totpAppMicrosoftAuthenticatorName"
+                },
+                WebAuthnPolicyRpEntityName = "keycloak",
+                WebAuthnPolicySignatureAlgorithms = new List<string> { "ES256", "RS256" },
+                WebAuthnPolicyRpId = "",
+                WebAuthnPolicyAttestationConveyancePreference = "not specified",
+                WebAuthnPolicyAuthenticatorAttachment = "not specified",
+                WebAuthnPolicyRequireResidentKey = "not specified",
+                WebAuthnPolicyUserVerificationRequirement = "not specified",
+                WebAuthnPolicyCreateTimeout = 0,
+                WebAuthnPolicyAvoidSameAuthenticatorRegister = false,
+                WebAuthnPolicyAcceptableAaguids = new List<string>(),
+                WebAuthnPolicyExtraOrigins = new List<string>(),
+                WebAuthnPolicyPasswordlessRpEntityName = "keycloak",
+                WebAuthnPolicyPasswordlessSignatureAlgorithms = new List<string> { "ES256", "RS256" },
+                WebAuthnPolicyPasswordlessRpId = "",
+                WebAuthnPolicyPasswordlessAttestationConveyancePreference = "not specified",
+                WebAuthnPolicyPasswordlessAuthenticatorAttachment = "not specified",
+                WebAuthnPolicyPasswordlessRequireResidentKey = "not specified",
+                WebAuthnPolicyPasswordlessUserVerificationRequirement = "not specified",
+                WebAuthnPolicyPasswordlessCreateTimeout = 0,
+                WebAuthnPolicyPasswordlessAvoidSameAuthenticatorRegister = false,
+                WebAuthnPolicyPasswordlessAcceptableAaguids = new List<string>(),
+                WebAuthnPolicyPasswordlessExtraOrigins = new List<string>(),
+                ClientProfiles = new ClientProfilesRepresentation
+                {
+                    Profiles = new List<ClientProfileRepresentation>(),
+                    AdditionalProperties = new Dictionary<string, object>()
+                },
+                BrowserSecurityHeaders = new Dictionary<string, string>
+                {
+                    {  "contentSecurityPolicyReportOnly", "" },
+                    {  "xContentTypeOptions", "nosniff" },
+                    {  "referrerPolicy", "no-referrer" },
+                    {  "xRobotsTag", "none" },
+                    {  "xFrameOptions", "SAMEORIGIN" },
+                    {  "contentSecurityPolicy", "frame-src \u0027self\u0027; frame-ancestors \u0027self\u0027; object-src \u0027none\u0027;" },
+                    {  "strictTransportSecurity", "max-age=31536000; includeSubDomains" },
+                },
+                BrowserFlow = "browser",
+                RegistrationFlow = "registration",
+                DirectGrantFlow = "direct grant",
+                ResetCredentialsFlow = "reset credentials",
+                ClientAuthenticationFlow = "clients",
+                DockerAuthenticationFlow = "docker auth",
+                FirstBrokerLoginFlow = "first broker login",
+                Attributes = new Dictionary<string, string>
+                {
+                    { "cibaBackchannelTokenDeliveryMode", "poll" },
+                    { "cibaExpiresIn", "120" },
+                    { "cibaAuthRequestedUserHint", "login_hint" },
+                    { "oauth2DeviceCodeLifespan", "600" },
+                    { "oauth2DevicePollingInterval", "5" },
+                    { "parRequestUriLifespan", "60" },
+                    { "cibaInterval", "5" },
+                    { "realmReusableOtpCode", "false" }
+                },
+                EventsEnabled = false,
+                EventsExpiration = 0,
+                EventsListeners = new List<string> { "jboss-logging" },
+                EnabledEventTypes = new List<string>(),
+                AdminEventsEnabled = false,
+                AdminEventsDetailsEnabled = false,
+                InternationalizationEnabled = false,
+                UserManagedAccessAllowed = false,
+                OrganizationsEnabled = false,
+                VerifiableCredentialsEnabled = false,
+                AdminPermissionsEnabled = false,
+                AdditionalProperties = new Dictionary<string, object>
+                {
+                    { "oauth2DeviceCodeLifespan",600}
+                }
+            };
         }
     }
 
